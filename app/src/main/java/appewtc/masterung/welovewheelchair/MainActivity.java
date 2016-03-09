89d374f21aac4dcb6ce7a45dc6b2ca99b802dcef
masterUNG <phrombutr@gmail.com>
2016-03-09 17:00:07 +0700

    Create Public Hub

--- app/src/main/java/appewtc/masterung/welovewheelchair/MainActivity.java
+++ app/src/main/java/appewtc/masterung/welovewheelchair/MainActivity.java
@@ -1,7 +1,8 @@
 package appewtc.masterung.welovewheelchair;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }   // Main Method
+
+    public void clickHub1(View view) {
+
     }
-}
+
+    public void clickHub2(View view) {
+
+    }
+
+    public void clickHub3(View view) {
+
+    }
+
+    public void clickHub4(View view) {
+
+    }
+
+}   // Main Class
