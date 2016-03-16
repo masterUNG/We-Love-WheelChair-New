c44380d5229194926195125ccc24c2f66eeff1e0
masterUNG <phrombutr@gmail.com>
2016-03-16 14:15:05 +0700

    ล่าสุด

--- app/src/main/java/appewtc/masterung/welovewheelchair/MainActivity.java
+++ app/src/main/java/appewtc/masterung/welovewheelchair/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.welovewheelchair;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,19 +14,20 @@ public class MainActivity extends AppCompatActivity {
     }   // Main Method
 
     public void clickHub1(View view) {
-
+        startActivity(new Intent(MainActivity.this, DetailWheelChairActivity.class));
     }
 
     public void clickHub2(View view) {
+        startActivity(new Intent(MainActivity.this, ShopWheelChairActivity.class));
 
     }
 
     public void clickHub3(View view) {
-
+        startActivity(new Intent(MainActivity.this, TravelWheelChairActivity.class));
     }
 
     public void clickHub4(View view) {
-
+        startActivity(new Intent(MainActivity.this, TripWheelChairActivity.class));
     }
 
 }   // Main Class
